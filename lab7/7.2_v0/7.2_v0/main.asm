652544a0b308c4f3bf64ab3980fbd0b680dc287c
herman-jaroslaw <[herman.jaroslaw@gmail.com]>
2021-04-26 21:36:52 +0200

    7.2_v0 finished

--- lab7/7.2_v0/7.2_v0/main.asm
+++ lab7/7.2_v0/7.2_v0/main.asm
@@ -10,7 +10,7 @@
 
 .org 0 
 	jmp main ;skip vector table 
-.org PCINT0addr
+.org PCINT0addr						;pocz¹tek programu ISR (procedury obs³ugi przerwania) dla tego przerwania
 	jmp int0_isr
 ;------- main ---------- 
 main: 
@@ -23,7 +23,7 @@ main:
 	sbi portb, 7 ;pull-up enable for portd.2 
 	ldi r20, (1<<pcint7) 
 	sts pcmsk0, r20 ;enable int0 
-	ldi r20, (1<<pcie3)										;dobrze czy nie 3 ???
+	ldi r20, (1<<pcie0)										;pcie0 ma byæ, nie wiem w sumie czemu
 	sts pcicr, r20
 	sei ;enable interrupts 
 stop: 
@@ -31,7 +31,7 @@ stop:
 ;------- int0 ISR ------- 
 int0_isr: 
 	in r21, pinb ;read portb
-	ldi r22, 0x20 
+	ldi r22, 0x20 ; HEX 20 = DEC 32 = BIN 0010 0000
 	eor r21, r22 ;toggle bit 5 
 	out portb,r21 
 	reti
