f66d04271272b404d33fa7c0135a86242262ce79
herman-jaroslaw <[herman.jaroslaw@gmail.com]>
2021-04-28 18:24:13 +0200

    created 7.3_PM

---
+++ lab7/7.3_v0/7.3_v0/7.3_v0_PM.asm
@@ -0,0 +1,86 @@
+;7.3_v0_PM
+
+.cseg 
+.org 0 
+		jmp start
+.org PCINT1addr													; ???
+		rjmp keypad_ISR ;Keypad External Interrupt Request
+		;.def button_code=r20													; ???
+;----------------------------------------------------------------------------------- 
+;Initialization
+start:
+		; Set Stack Pointer to top of RAM
+		ldi r16, high(ramend) 
+		out SPH, r16 
+		ldi r16, low(ramend) 
+		out SPL, r16
+
+		;SET UP 6 LEDS 
+		;Set up port B as output for LED controls 
+		ldi r16, 0xFF 
+		out ddrb, r16 
+		ldi r20, 0x0f 
+		out ddrc, r20
+
+		;Set rows to high (pull ups) and columns to low 
+		ldi r20, 0x30 
+		out portc, r20
+
+		;Select rows as interrupt triggers 
+		ldi r20, (1<<pcint12)|(1<<pcint13)						;12 i 13 dobre ???
+		sts pcmsk1, r20
+
+
+
+		;Enable pcint1 
+		ldi r20, (1<<pcie1) 
+		sts pcicr, r20
+
+		;Reset register for output 
+		ldi r18, 0x00
+		;Global Enable Interrupt
+		Sei
+;----------------------------------------------------------------------------------- 
+;Set up infinite loop 
+loop:
+		call led_display 
+		rjmp loop
+;----------------------------------------------------------------------------------- 
+;Keypad Interrupt Service Routine 
+keypad_ISR:
+		;Set rows as outputs and columns as inputs 
+		ldi r20, 0x30	//???????????												
+		out ddrc, r20
+
+		;Set columns to high (pull ups) and rows to low 
+		ldi r20, 0x30	///???????????????????								; j. w. 
+		out portc, r20
+
+		;Read Port C. Columns code in low nibble 
+		in r16, pinc
+
+		;Store columns code to r18 on low nibble 
+		mov r18, r16 
+		andi r18, 0x0f
+
+		;Set rows as inputs and columns as outputs 
+		ldi r20, 0x0f	//???????????????										; j. w. 
+		out ddrc, r20
+
+		;Set rows to high (pull ups) and columns to low 
+		ldi r20, 0x0f	//???????????										; j. w.
+		out portc, r20
+
+		;Read Port C. Rows code in high nibble 
+		in r16, pinc
+
+		;Merge with previous read 
+		andi r16, 0x30 
+		add r18, r16
+
+		reti 
+;----------------------------------------------------------------------------------- 
+;display value from r18 on leds 
+led_display: 
+	out portb,r18 
+	ret
